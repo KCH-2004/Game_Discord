81c17bb0d6a950c4716902af9aa166923f0eebd2
Kevin <kevin-chen2004@outlook.com>
2026-09-03 18:34:27 +0200

    Initiation des stats et servant

--- persos.py
+++ persos.py
@@ -4,29 +4,56 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 class Stats:
-    def __init__(self,attaque,magie,defense,mdefense,vitesse,mana,vie):
-        if type(attaque) == int and attaque > 0:
-            self.attaque = attaque
-        if type(magie) == int and magie> 0:
-            self.magie = magie
-        if type(mdefense) == int and mdefense > 0:
-            self.mdefense = mdefense
-        if type(defense) == int and defense > 0:
-            self.defense = defense
-        if type(vitesse) == int and vitesse > 0:
-            self.vitesse = vitesse
-        if type(mana) == int and mana > 0:
-            self.mana = mana
-        if type(vie) == int and vie > 0:
-            self.vie = vie
+
+    def __init__(self, attaque, magie, defense, mdefense, vitesse, mana, vie):
+        stats = {
+            "attaque": attaque,
+            "magie": magie,
+            "defense": defense,
+            "mdefense": mdefense,
+            "vitesse": vitesse,
+            "mana": mana,
+            "vie": vie
+        }
+
+        for name, value in stats.items():
+            if type(value) == int and value > 0:
+                setattr(self, name, value)
+            else:
+                raise ValueError(f"Type ou valeur de {name} incorrecte")
+
         self.status = "Alive"
+
+    def getAtq(self):
+        return self.attaque
+
+    def getmag(self):
+        return self.magie
+
+    def getdef(self):
+        return self.defense
+
+    def getmdef(self):
+        return self.mdefense
+
+    def getvit(self):
+        return self.vitesse
+
+    def getmana(self):
+        return self.mana
+
+    def getvie(self):
+        return self.vie
 
 class Classe:
     def __init__(self,stats,nom):
         self.stats = stats
         self.nom = nom
 
-class Location:
+    def getstats(self):
+        return self.stats
+
+class Localisation:
     def __init__(self, nom,listemasters,listeservants):
         pass
 
@@ -36,6 +63,26 @@ class Master:
 
 class Servant:
     def __init__(self,nom,classe,stats,nommaster,personnalite,confiance):
-        pass
+        strings = {"nom": nom,
+                   "nommaster": nommaster,
+                   "personnalite": personnalite,
+                   "confiance": confiance}
+        if not isinstance(classe,Classe):
+            raise TypeError("Type classe invalide")
+        else:
+            self.classe = classe
 
+        if not isinstance(stats,Stats):
+            raise TypeError("Type de stats invalide")
+        else:
+            self.stats = stats
 
+        for nameArg,arg in strings.items():
+            if isinstance(arg,str):
+                setattr(self, nameArg, arg)
+            else:
+                raise TypeError(f"Type de {arg} invalide")
+
+    def setFinalStats(self):
+        for args in self.stats:
+            newstat = args + self.classe.getstats()
