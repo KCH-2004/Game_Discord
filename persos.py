6fe75d3ce6a3a8e693078aac581be1da73e7e9f2
Kevin <kevin-chen2004@outlook.com>
2026-09-04 15:07:24 +0200

    Amélioration et ajout des ordres / action

--- persos.py
+++ persos.py
@@ -1,88 +1,171 @@
-# This is a sample Python script.
+from enum import Enum
+from dataclasses import dataclass
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+class NiveauConfiance(Enum):
+    #Pourcentage de trahison selon la confiance (modifié selon la personnalité du servant)
+    ABSOLUE = 0
+    HAUTE = 5
+    MOYENNE = 30
+    BASSE = 75
+    NULLE = 100
+
+    def augmenterConfiance(self):
+        confiance = list(NiveauConfiance)
+        niveau_confiance_servant = confiance.index(self)
+
+        if not niveau_confiance_servant == 0:
+            return confiance[niveau_confiance_servant - 1]
+        else:
+            return NiveauConfiance.ABSOLUE
+
+    def baisserConfiance(self):
+        confiance = list(NiveauConfiance)
+        niveau_confiance_servant = confiance.index(self)
+
+        if not niveau_confiance_servant == 4:
+            return confiance[niveau_confiance_servant + 1]
+        else:
+            return NiveauConfiance.NULLE
+
+@dataclass
+class Personnalite:
+    trait: str
+    multiplicateur_trahison: float
+
+@dataclass
 class Stats:
+    attaque: int
+    magie: int
+    defense: int
+    mdefense: int
+    vitesse: int
+    mana: int
+    vie: int
 
-    def __init__(self, attaque, magie, defense, mdefense, vitesse, mana, vie):
-        stats = {
-            "attaque": attaque,
-            "magie": magie,
-            "defense": defense,
-            "mdefense": mdefense,
-            "vitesse": vitesse,
-            "mana": mana,
-            "vie": vie
-        }
+    def __iadd__(self, otherstats):
+        if not isinstance(otherstats, Stats):
+            raise TypeError("Type de Otherstats invalide\n"
+                            "class: Stats")
 
-        for name, value in stats.items():
-            if type(value) == int and value > 0:
-                setattr(self, name, value)
-            else:
-                raise ValueError(f"Type ou valeur de {name} incorrecte")
+        self.attaque += otherstats.attaque
+        self.magie += otherstats.magie
+        self.defense += otherstats.defense
+        self.mdefense += otherstats.mdefense
+        self.vitesse += otherstats.vitesse
+        self.mana += otherstats.mana
+        self.vie += otherstats.vie
 
-        self.status = "Alive"
+        return self
 
-    def getAtq(self):
-        return self.attaque
-
-    def getmag(self):
-        return self.magie
-
-    def getdef(self):
-        return self.defense
-
-    def getmdef(self):
-        return self.mdefense
-
-    def getvit(self):
-        return self.vitesse
-
-    def getmana(self):
-        return self.mana
-
-    def getvie(self):
-        return self.vie
-
+@dataclass
 class Classe:
-    def __init__(self,stats,nom):
-        self.stats = stats
-        self.nom = nom
+    stats: Stats
+    nom: str
 
-    def getstats(self):
-        return self.stats
+@dataclass
+class Participants:
+    nom: str
+    stats: Stats
+    sceauxrestant: int
+    encombat: bool
+    statut:str
 
-class Localisation:
-    def __init__(self, nom,listemasters,listeservants):
+@dataclass
+class Master(Participants):
+    nomservant: str
+
+@dataclass
+class Servant(Participants):
+    classe:Classe
+    nommaster:str
+    personnalite:Personnalite
+    confiance:NiveauConfiance
+    noblefantasm:str
+
+    def setfinalstats(self):
+        self.stats += self.classe.stats
+
+class Carte:
+
+    def __init__(self, listelocalisation:list,dictlocalisationmaster:dict,dictlocalisationservant:dict):
+        if not all(isinstance(l, str) for l in listelocalisation):
+            raise TypeError("Erreur de Localisation dans la Carte.")
+        else:
+            self.listelocalisation = listelocalisation
+
+        if not all(isinstance(m,Master) and isinstance(l,str) for m,l in dictlocalisationmaster.items()):
+            raise TypeError("Erreur de Localisation des Master dans la Carte.")
+        else:
+            self.dictlocalisationmaster = dictlocalisationmaster
+
+        if not all(isinstance(s,Servant) and isinstance(l,str) for s,l in dictlocalisationservant.items()):
+            raise TypeError("Erreur de Localisation des Servant dans la Carte.")
+        else:
+            self.dictlocalisationservant = dictlocalisationservant
+
+    def deplacermaster(self,master,nouvellelocalisation):
+        for l in self.listelocalisation:
+            if nouvellelocalisation == l:
+                self.dictlocalisationmaster[master] = l
+                return
+        raise TypeError(f"Erreur de deplacement Master {master} sur la Carte")
+
+    def deplacerservant(self,servant,nouvellelocalisation):
+        for l in self.listelocalisation:
+            if nouvellelocalisation == l:
+                self.dictlocalisationservant[servant] = l
+                return
+        raise TypeError(f"Erreur de deplacement Servant {servant} sur la Carte")
+
+
+class Action:
+
+    def __init__(self, initiateur:Participants):
+        self.initiateur = initiateur
+        self.priorite = 0
+
+    def executer(self, carte:Carte):
+        raise NotImplementedError("Cette action n'a pas de logique d'exécution")
+
+class Attaque(Action):
+    def __init__(self,initiateur:Participants,cible:Participants):
         pass
 
-class Master:
-    def __init__(self,nom,stats,nomservant):
+class Scout(Action):
+    def __init__(self,initiateur:Participants):
         pass
 
-class Servant:
-    def __init__(self,nom,classe,stats,nommaster,personnalite,confiance):
-        strings = {"nom": nom,
-                   "nommaster": nommaster,
-                   "personnalite": personnalite,
-                   "confiance": confiance}
-        if not isinstance(classe,Classe):
-            raise TypeError("Type classe invalide")
-        else:
-            self.classe = classe
+class Ultime(Action):
+    def __init__(self,initiateur:Participants):
+        pass
 
-        if not isinstance(stats,Stats):
-            raise TypeError("Type de stats invalide")
-        else:
-            self.stats = stats
+class Sacrifice(Action):
+    def __init__(self,initiateur:Participants):
+        pass
 
-        for nameArg,arg in strings.items():
-            if isinstance(arg,str):
-                setattr(self, nameArg, arg)
-            else:
-                raise TypeError(f"Type de {arg} invalide")
+class Retraite(Action):
+    def __init__(self,initiateur:Participants):
+        pass
 
-    def setFinalStats(self):
-        for args in self.stats:
-            newstat = args + self.classe.getstats()
+class OrdreAbsolue(Action):
+    def __init__(self,initiateur:Master,cible:Servant,ordre:str):
+        super().__init__(initiateur)
+        self.cible = cible
+        self.ordre = ordre
+
+    def executer(self,carte:Carte):
+        if self.initiateur.sceauxrestant <= 0:
+            return False
+
+        self.initiateur.sceauxrestant -= 1
+        self.cible.confiance = self.cible.confiance.baisserConfiance()
+        return True
+
+class Suicide(OrdreAbsolue):
+    def __init__(self,initiateur:Master,cible:Servant):
+        super().__init__(initiateur,cible)
+
+
+    def executer(self, carte:Carte):
+        pass
